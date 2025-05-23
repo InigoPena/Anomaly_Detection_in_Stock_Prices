1b8603b9e81b0e2a706b8d4a8fe52782cc412488
InigoPena <i.pena@opendeusto.es>
2025-05-23 11:16:48 +0200

    Final Commit

--- src/data_loading.py
+++ src/data_loading.py
@@ -7,19 +7,22 @@ apple = yf.Ticker("AAPL")
 nvidia = yf.Ticker("NVDA")
 google = yf.Ticker("GOOGL")
 tesla = yf.Ticker("TSLA")
+salesforce = yf.Ticker("CRM")
 
 msft_data = msft.history(period="20y")
 apple_data = apple.history(period="20y")
 nvidia_data = nvidia.history(period="20y")
 google_data = google.history(period="20y")
 tesla_data = tesla.history(period="20y")
+salesforce_data = salesforce.history(period="20y")
 
 tickers = {
     "MSFT": msft_data,
     "AAPL": apple_data,
     "NVDA": nvidia_data,
     "GOOGL": google_data,
-    "TSLA": tesla_data
+    "TSLA": tesla_data,
+    "CRM": salesforce_data
 }
 
 # Selecting Close, Open, High, Low, Volume
@@ -42,4 +45,4 @@ for ticker in tickers:
 
 # Saving data to CSV
 for ticker in tickers:
-    tickers[ticker].to_csv(f"data/processed/{ticker}_data.csv")
+    tickers[ticker].to_csv(f"data/processed_data/{ticker}_data.csv")
